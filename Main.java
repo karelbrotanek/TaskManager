e994e63f0743a7ded7ed4416e22c0b8618d496db
karelbrotanek <73097585+karelbrotanek@users.noreply.github.com>
2020-11-09 17:04:12 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {0,9,5,10,-5,6,99,5};
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+
+        }
+    }
+}
